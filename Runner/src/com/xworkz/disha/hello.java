45cd0a84db6051aa739a92fe77e6dc76e62e7193
Disha <dishaacharyaa04@gmail.com>
2025-03-18 15:16:46 +0530

    classes as parameters

--- Runner/src/com/xworkz/disha/hello.java
+++ Runner/src/com/xworkz/disha/hello.java
@@ -1,7 +1,7 @@
 package com.xworkz.disha;
 
-public class boring {
+public class hello {
     public static void main(String[] args){
-        System.out.println("class is boring");
+        System.out.println("hello world");
     }
 }
