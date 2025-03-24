be5a2de6cb8c05b4532de4954c69165e0a84705d
Disha <dishaacharyaa04@gmail.com>
2025-03-24 20:39:04 +0530

    Entering the code

---
+++ Demoo/src/com/xworkz/Demoo/Main.java
@@ -0,0 +1,10 @@
+package com.xworkz.Demoo;
+
+public class Main {
+    public static void main(String[] args) {
+        Demo obj = new Demo();
+        obj.publicMethod();
+        obj.packageMethod();
+        obj.accessPrivateMethod();
+    }
+}
